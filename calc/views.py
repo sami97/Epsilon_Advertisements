ed0e0b204f05b91bddeaeeeaeee1edacd790256e
Samyak Jain <26330167+sami97@users.noreply.github.com>
2021-02-01 16:57:13 +0530

    Add files via upload

---
+++ calc/views.py
@@ -0,0 +1,13 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+   return render(request, 'home.html', {'name': 'Epsilon'})
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2
+    return render(request, 'result.html',{'result':res})
